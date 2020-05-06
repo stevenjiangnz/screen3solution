7ba7962c4493675fdf3b53ac324dcee81f55b9c3
Steven Jiang <stevenjiangnz@gmail.com>
2020-05-06 10:19:16 +1000

    load ticker ok

--- screen3_data_loader/src/screen3_data_loader/controllers/DataLoadController.cs
+++ screen3_data_loader/src/screen3_data_loader/controllers/DataLoadController.cs
@@ -4,11 +4,14 @@ using System.IO.Compression;
 using System.Threading.Tasks;
 using System.Collections.Generic;
 using Amazon.Lambda.Core;
+using System.Globalization;
 using Amazon.S3;
 using Amazon.S3.Model;
-using Newtonsoft.Json;
 using Screen3.S3Service;
+using Screen3.Entity;
 using Screen3.Utils;
+using Screen3.DynamoService;
+using CsvHelper;
 
 namespace screen3_data_loader.controllers
 {
@@ -17,28 +20,42 @@ namespace screen3_data_loader.controllers
 
         private string S3_Bucket_Name;
         private string Temp_Folder;
+        private List<StockEntity> stockList;
+        private StockServiceDAL dal;
+
 
         public DataLoadController()
         {
             this.S3_Bucket_Name = Environment.GetEnvironmentVariable("SCREEN3_S3_BUCKET");
             this.Temp_Folder = Environment.GetEnvironmentVariable("SCREEN3_TEMP_FOLDER");
+            string asx300TableName = Environment.GetEnvironmentVariable("SCREEN3_ASX300_TABLE_NAME");
+            this.dal = new StockServiceDAL(asx300TableName);
         }
+
         public async Task<String> StartProcessAsync()
         {
+            this.stockList = await this.dal.GetAll();
+
+            // retrieve all stock list.
+            this.stockList = await this.dal.GetAll();
+
             string result = "";
 
             List<S3Object> fileList = await this.GetSourceFileListAsync(this.S3_Bucket_Name, "source");
 
             LambdaLogger.Log($"found files in the source folder, {ObjectHelper.ToJson(fileList)}\n");
 
-            foreach (var fileInfo in fileList) {
-                if (fileInfo.Size > 0) {
+            foreach (var fileInfo in fileList)
+            {
+                if (fileInfo.Size > 0)
+                {
                     LambdaLogger.Log($"About to download file, {fileInfo.Key}\n");
                     String resultFileName = await this.DownloadFileAsync(fileInfo.BucketName, fileInfo.Key, this.Temp_Folder + "originSourceFiles/");
 
                     string dailyTargetFolder = this.Temp_Folder + $"originExtractedFiles/{fileInfo.Key}/";
                     LambdaLogger.Log($"Download file, {fileInfo.Key}, about to extract to {dailyTargetFolder}.\n");
-                    var dailyFileList = this.ExtractIntoDayData(resultFileName, dailyTargetFolder);
+                    List<String> dailyFileList = this.ExtractIntoDayData(resultFileName, dailyTargetFolder);
+
                     LambdaLogger.Log($"Extracted files done. {ObjectHelper.ToJson(dailyFileList)}\n");
                 }
             }
@@ -46,7 +63,8 @@ namespace screen3_data_loader.controllers
             return result;
         }
 
-        public List<String>  ExtractIntoDayData(string fileName, string targetPath) {
+        public List<String> ExtractIntoDayData(string fileName, string targetPath)
+        {
             List<String> resultFileList = new List<String>();
 
             FileHelper.ClearDirectory(targetPath, true);
@@ -75,6 +93,37 @@ namespace screen3_data_loader.controllers
             String resultFileName = await service.DownloadFileFromS3Async(bucketName, keyName, tempFolder);
 
             return resultFileName;
+        }
+
+        public void AddDailyFileIntoStockDict(string path)
+        {
+
+        }
+
+        public List<TickerEntity> LoadTickerFromCSV(string path)
+        {
+            using (var reader = new StreamReader(path))
+            using (var csv = new CsvReader(reader, CultureInfo.InvariantCulture))
+            {
+                var tickers = new List<TickerEntity>();
+                // csv.Read();
+                while (csv.Read())
+                {
+                    var ticker = new TickerEntity();
+
+                    ticker.code = csv.GetField<string>(0);
+                    ticker.period = csv.GetField<int>(1);
+                    ticker.open = csv.GetField<float>(2);
+                    ticker.high = csv.GetField<float>(3);
+                    ticker.low = csv.GetField<float>(4);
+                    ticker.close = csv.GetField<float>(5);
+                    ticker.volume = csv.GetField<long>(6);
+
+                    tickers.Add(ticker);
+                }
+
+                return tickers;
+            }
         }
 
     }
--- screen3_data_loader/test/screen3_data_loader.Tests/controllers/DataLoadControllerTest.cs
+++ screen3_data_loader/test/screen3_data_loader.Tests/controllers/DataLoadControllerTest.cs
@@ -8,6 +8,7 @@ using Amazon.Lambda.Core;
 using Amazon.Lambda.TestUtilities;
 
 using screen3_data_loader.controllers;
+using Screen3.Utils;
 
 namespace screen3_data_loader.controllers.Tests
 {
@@ -35,6 +36,18 @@ namespace screen3_data_loader.controllers.Tests
             DataLoadController controller = new DataLoadController();
 
             controller.ExtractIntoDayData("/tmp/screen3_temp_files/originSourceFiles/2007-2012.zip", "/tmp/screen3_temp_files/originExtractedFiles/");
+        }
+
+
+        [Fact]
+        public void TestLoadTickerFromCSV()
+        {
+            DataLoadController controller = new DataLoadController();
+            string path = @"/tmp/screen3_temp_files/originExtractedFiles/source/1997-2006.zip/1997-2006/20061229.TXT";
+
+            string tickerString = ObjectHelper.ToJson(controller.LoadTickerFromCSV(path));
+
+            Console.WriteLine(tickerString);
         }
     }
 }
