fecd7f0f2e2ac15644ad847dccef078df4951ed3
Steven Jiang <stevenjiangnz@gmail.com>
2020-05-11 11:57:56 +1000

    macd done

---
+++ Screen3.Indicator/MACD.cs
@@ -0,0 +1,58 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Screen3.Indicator
+{
+    public class MACD
+    {
+        public static Result Calculate(double[] inputData, int periodSlow, int periodFast, int periodSignal, double?[] outMACD, double?[] outSignal, double?[] outHist)
+        {
+            Result result = new Result();
+            result.Status = ResultStatus.Success;
+
+            int len = inputData.Length;
+
+            double?[] emaSlow = new double?[len];
+            double?[] emaFast = new double?[len];
+
+            EMA.Calculate(inputData, periodSlow, 2.0, emaSlow);
+            EMA.Calculate(inputData, periodFast, 2.0, emaFast);
+
+            for (int i = periodSlow - 1; i < len; i++)
+            {
+                if (emaFast[i].HasValue && emaSlow[i].HasValue)
+                {
+                    outMACD[i] = emaFast[i].Value - emaSlow[i].Value;
+                }
+            }
+
+            int tmpSLength = len - periodSlow + 1;
+            double[] sInput = new double[tmpSLength];
+
+            for (int j = 0; j < tmpSLength; j++)
+            {
+                sInput[j] = outMACD[periodSlow - 1 + j].Value;
+            }
+
+            double?[] tmpOutS = new double?[tmpSLength];
+
+            EMA.Calculate(sInput, periodSignal, 2.0, tmpOutS);
+
+            for (int k = periodSignal - 1; k < tmpSLength; k++)
+            {
+                outSignal[k + periodSlow - 1] = tmpOutS[k].Value;
+            }
+
+            for (int m = periodSlow + periodSignal - 2; m < len; m++)
+            {
+                if(outMACD[m].HasValue && outSignal[m].HasValue)
+                outHist[m] = outMACD[m].Value - outSignal[m].Value;
+            }
+
+            return result;
+        }
+    }
+}
---
+++ Screen3.Test/Indicator/MACDTest.cs
@@ -0,0 +1,45 @@
+using System;
+using System.Collections.Generic;
+using Xunit;
+using Screen3.Indicator;
+using Screen3.Utils;
+using Screen3.BLL;
+using Screen3.Entity;
+
+namespace Screen3.Test.Indicator
+{
+    public class MACDTest
+    {
+        private string s3_bucket_name = "stevenjiangnz-screen3-eod-source";
+        private string tempTickerFolder = "/tmp/screen3_temp_files/localticker/";
+
+
+        [Fact]
+        public async void TestMACD()
+        {
+            TickerBLL bll = new TickerBLL(this.s3_bucket_name);
+            List<TickerEntity> tList = await bll.GetDailyTickerEntityList("ORG", this.tempTickerFolder, 20181001, 20191101);
+
+            double[] inputData = new double[tList.Count];
+            
+            var i = 0;
+            foreach (var t in tList)
+            {
+                inputData[i] = t.C;
+                i++;
+            }
+
+            double?[] m = new double?[tList.Count];
+            double?[] s = new double?[tList.Count];
+            double?[] h = new double?[tList.Count];
+
+            Result res = MACD.Calculate(inputData, 26, 12, 9, m, s, h);
+
+            var tArray = tList.ToArray();
+            for (int j = 0; j< tList.Count; j++) {
+                Console.WriteLine($"{tArray[j].T} {tArray[j].P} O: {tArray[j].O} H: {tArray[j].H} L: {tArray[j].L} C: {tArray[j].C} M: {m[j]} S: {s[j]} H: {h[j]} ");
+            }
+        }
+
+    }
+}
