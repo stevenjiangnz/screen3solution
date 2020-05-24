fce4ac868fbc75c410d26f05d3ed9423169ab5fb
Steven Jiang <stevenjiangnz@gmail.com>
2020-05-24 16:23:40 +1000

    basic menu link works

--- screen3_web/src/App.js
+++ screen3_web/src/App.js
@@ -2,15 +2,24 @@ import React, { Component } from "react";
 import TopNav from "./component/TopNav";
 import Stock from "./component/stock/Stock";
 import Screen from "./component/screen/Screen";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 export class App extends Component {
   render() {
     return (
-      <div>
-        <TopNav />
-        <Stock />
-        <Screen />
-      </div>
+      <Router>
+        <div>
+          <TopNav />
+          <Switch>
+            <Route exact path="/">
+              <Stock />
+            </Route>
+            <Route exact path="/screen">
+              <Screen />
+            </Route>
+          </Switch>
+        </div>
+      </Router>
     );
   }
 }
--- screen3_web/src/component/TopNav.js
+++ screen3_web/src/component/TopNav.js
@@ -1,8 +1,21 @@
 import React, { Component } from "react";
+import { Link } from "react-router-dom";
 
 export class TopNav extends Component {
   render() {
-    return <div>thisi s the top Nav</div>;
+    return (
+      <div>
+        thisi s the top Nav
+        <ul>
+          <li>
+            <Link to="/">Stock</Link>
+          </li>
+          <li>
+            <Link to="/screen">Screen</Link>
+          </li>
+        </ul>
+      </div>
+    );
   }
 }
 
