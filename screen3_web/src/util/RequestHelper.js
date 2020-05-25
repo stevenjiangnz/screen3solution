811052fcaa7affe9ec5e4638815a1d601d933b6c
Steven Jiang <stevenjiangnz@gmail.com>
2020-05-25 16:46:55 +1000

    make cors works

--- screen3_web/src/util/RequestHelper.js
+++ screen3_web/src/util/RequestHelper.js
@@ -4,22 +4,10 @@ import axios from "axios";
 
 export class RequestHelper {
   constructor() {
-    console.log(
-      "request helper constrcutor called: ",
-      process.env.REACT_APP_API_BASE_URL,
-      process.env.REACT_APP_API_ACCESS_KEY
-    );
-
     axios.defaults.baseURL = process.env.REACT_APP_API_BASE_URL;
     axios.defaults.headers.common["x-api-key"] =
       process.env.REACT_APP_API_ACCESS_KEY;
     axios.defaults.headers.common["Content-Type"] = "application/json";
-    axios.defaults.headers.common["Access-Control-Allow-Origin"] = "*";
-    // axios.defaults.headers.common["Access-Control-Allow-Methods"] =
-    //   "DELETE, POST, PUT, GET, OPTIONS";
-    // axios.defaults.headers.common["Access-Control-Allow-Headers"] =
-    //   "Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With, x-api-key";
-    // axios.defaults.headers.common["Access-Control-Allow-Credentials"] = true;
   }
 
   getIntance() {
