3d85afa55c55bc173c63d5477563734b6ad50569
Steven Jiang <stevenjiangnz@gmail.com>
2020-05-26 09:40:17 +1000

    before add aggrid

--- screen3_web/src/component/stock/Stock.js
+++ screen3_web/src/component/stock/Stock.js
@@ -1,12 +1,19 @@
 import React, { Component } from "react";
 import StockList from "./StockList";
+import StockDetail from "./StockDetail";
 
 export class Stock extends Component {
   render() {
     return (
-      <div>
-        this is the stock page...
-        <StockList />
+      <div id="main-panel" className="container-fluid">
+        <div className="row">
+          <div className="col-sm-3">
+            <StockList />
+          </div>
+          <div className="col-sm-9">
+            <StockDetail></StockDetail>
+          </div>
+        </div>
       </div>
     );
   }
---
+++ screen3_web/src/component/stock/StockDetail.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function StockDetail() {
+  return <div>this the StockDetail panel</div>;
+}
+
+export default StockDetail;
