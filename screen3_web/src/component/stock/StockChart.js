bbdb0cd78cfdb2645986bb34db8d10ea26600714
Steven Jiang <stevenjiangnz@gmail.com>
2020-05-28 12:55:58 +1000

    candle displayed

--- screen3_web/src/component/stock/StockChart.js
+++ screen3_web/src/component/stock/StockChart.js
@@ -2,48 +2,82 @@ import React, { Component } from "react";
 import AppContext from "../../Context";
 import Highcharts from "highcharts/highstock";
 import HighchartsReact from "highcharts-react-official";
+import TickerServer from "../../service/TickerService";
 
 export class StockChart extends Component {
   context;
   chartRef;
+  chart;
+  tickerService;
+  groupingUnits;
 
   constructor(props) {
     super(props);
+
+    this.tickerService = new TickerServer();
     this.chartRef = React.createRef();
 
-    this.state = {};
-  }
-
-  render() {
-    const options = {
+    this.groupingUnits = [
+      [
+        "week", // unit name
+        [1], // allowed multiples
+      ],
+      ["month", [1, 2, 3, 4, 6]],
+    ];
+    this.state = {
       chart: {
         marginRight: 20,
         marginLeft: 10,
       },
-      title: {
-        text: "My stock chart",
-      },
       series: [
         {
-          data: [1, 2, 1, 4, 3, 6, 7, 3, 8, 6, 9],
+          type: "candlestick",
+          data: [],
+          groupingUnits: this.groupingUnits,
         },
       ],
-    };
+      rangeSelector: {
+        selected: 1,
+      },
 
+      screen_setting: "",
+    };
+  }
+
+  componentDidMount() {
+    this.chart = this.chartRef.current.chart;
+    this.prepareDrawChart();
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    this.prepareDrawChart();
+  }
+
+  prepareDrawChart = async () => {
+    const stock = this.context.state.selectedStock;
+
+    const tickers = await this.tickerService.getTickerList(stock.code);
+
+    this.chart.setTitle({ text: `${stock.code} - ${stock.company}` });
+
+    this.chart.series[0].setData(tickers);
+    this.chart.series[0].name = stock.code;
+  };
+
+  render() {
     return (
       <AppContext.Consumer>
         {(context) => {
           this.context = context;
-
           return (
             <>
-              <div className="row">{JSON.stringify(this.context)}</div>
               <div className="chart-wrapper">
                 <div className="chart-inner">
                   <HighchartsReact
                     highcharts={Highcharts}
                     constructorType={"stockChart"}
-                    options={options}
+                    options={this.state}
+                    ref={this.chartRef}
                   />
                 </div>
               </div>
--- screen3_web/src/service/TickerService.js
+++ screen3_web/src/service/TickerService.js
@@ -1,3 +1,17 @@
-export class TickerService {}
+import RequestHelper from "../util/RequestHelper";
+
+export class TickerService {
+  async getTickerList(code, type = "day", start = 0, end = 0) {
+    const tickers = [];
+    const req = new RequestHelper().getIntance();
+    const resp = await req.get(`ticker/${code}`);
+
+    resp.data.forEach((t) => {
+      tickers.push([t.p_Stamp, t.o, t.h, t.l, t.c]);
+    });
+    console.log(`${code} - ${tickers.length}`);
+    return tickers;
+  }
+}
 
 export default TickerService;
