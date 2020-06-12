02342f697f1ee1f4037255086700a8c841f3c32c
Steven Jiang <stevenjiangnz@gmail.com>
2020-06-12 15:17:24 +1000

    about to improve loading

--- screen3_web/src/component/screen/trade/TradeAccount.js
+++ screen3_web/src/component/screen/trade/TradeAccount.js
@@ -1,7 +1,6 @@
 import React, { Component } from "react";
 import { v4 as uuidv4 } from "uuid";
 import TradeService from "../../../service/TradeService";
-import TickerService from "../../../service/TickerService";
 
 export class TradeAccount extends Component {
   tradeService;
--- screen3_web/src/component/screen/trade/TradingPanel.js
+++ screen3_web/src/component/screen/trade/TradingPanel.js
@@ -1,8 +1,62 @@
 import React, { Component } from "react";
+import TradeService from "../../../service/TradeService";
 
 export class TradingPanel extends Component {
+  tradeService;
+  constructor(props) {
+    super(props);
+
+    this.tradeService = new TradeService();
+    this.state = {
+      accounts: [],
+      selectedAccountId: {},
+    };
+  }
+
+  handleAccountChange = (e) => {
+    this.setState({
+      selectedAccountId: e.target.value,
+    });
+  };
+
+  componentDidMount() {
+    this.tradeService.getAllAccount().then((resp) => {
+      this.setState({
+        accounts: resp.data,
+      });
+    });
+  }
+
   render() {
-    return <div>trading panel</div>;
+    return (
+      <div style={{ paddingTop: 10, paddingLeft: 2, paddingRight: 2 }}>
+        <div class="form-group">
+          <select
+            class="form-control"
+            id="accountSelector"
+            value={this.state.selectedAccountId}
+            onChange={this.handleAccountChange}
+          >
+            {this.state.accounts.map((account) => {
+              return <option value={account.id}>{account.name}</option>;
+            })}
+          </select>
+        </div>
+        <div>
+          <button type="button" class="btn btn-success btn-sm">
+            Long
+          </button>
+          <button
+            type="button"
+            class="btn btn-danger btn-sm"
+            style={{ marginLeft: 10 }}
+          >
+            Short
+          </button>
+        </div>
+        {/* {JSON.stringify(this.state.selectedAccountId)} */}
+      </div>
+    );
   }
 }
 
