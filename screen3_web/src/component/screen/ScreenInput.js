3108eae1b5307a1f184f0829c7ed828c97ebf60d
Steven Jiang <stevenjiangnz@gmail.com>
2020-06-15 16:17:40 +1000

    about to refactor screen service

--- screen3_web/src/component/screen/ScreenInput.js
+++ screen3_web/src/component/screen/ScreenInput.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import screenMACDWilliam from "../../features/screen_macd_willaim";
+import screenADX from "../../features/screen_adx";
 import ScreenService from "../../service/ScreenService";
 import AppContext from "../../Context";
 
@@ -13,6 +14,7 @@ export class ScreenInput extends Component {
     this.state = {
       screenRequest: JSON.stringify(screenMACDWilliam, undefined, 4),
       isLoading: false,
+      selectedScreen: "macd_william",
     };
   }
 
@@ -21,6 +23,24 @@ export class ScreenInput extends Component {
   updateScreenRequest = (e) => {
     this.setState({
       screenRequest: e.target.value,
+    });
+  };
+
+  onScreenChange = (e) => {
+    var defaultRequest;
+    console.log("e.target.value: ", e.target.value);
+    switch (e.target.value) {
+      case "macd_william":
+        defaultRequest = JSON.stringify(screenMACDWilliam, undefined, 4);
+        break;
+      case "adx":
+        defaultRequest = JSON.stringify(screenADX, undefined, 4);
+        break;
+    }
+
+    this.setState({
+      screenRequest: defaultRequest,
+      selectedScreen: e.target.value,
     });
   };
 
@@ -75,6 +95,22 @@ export class ScreenInput extends Component {
           this.context = context;
           return (
             <div style={{ marginTop: 10, marginBottom: 10 }}>
+              <p>{this.state.selectedScreen}</p>
+              <div className="form-group">
+                <select
+                  className="form-control"
+                  id="accountSelector"
+                  value={this.state.selectedScreen}
+                  onChange={this.onScreenChange}
+                >
+                  <option key="1" value="macd_william">
+                    Screen Macd William %R
+                  </option>
+                  <option key="2" value="adx">
+                    Screen ADX
+                  </option>
+                </select>
+              </div>
               <div className="form-group">
                 <textarea
                   className="form-control screen-request-input"
